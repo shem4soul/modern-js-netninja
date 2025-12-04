ef8c7d63245b272c78260f9a07ed4b83a1548022
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-04 19:43:54 +0100

    refactor: comment out date and time logging code for clarity

--- chapter_11/app.js
+++ chapter_11/app.js
@@ -1,23 +1,28 @@
-//dates & times
+// //dates & times
+// const now = new Date();
+// console.log(now);
+// console.log(typeof now);
+
+// console.log('getFullYear:', now.getFullYear());
+// console.log('getMonth:', now.getMonth());
+// console.log('getDate:', now.getDate());
+// console.log('getDay:', now.getDay());
+// console.log('getHours:', now.getHours());
+// console.log('getMinutes:', now.getMinutes());
+// console.log('getSeconds:', now.getSeconds());
+// console.log('getMilliseconds:', now.getMilliseconds());
+// console.log('getTime:', now.getTime());
+
+// // timestamps
+// console.log('timestamp:', now.getTime());
+
+// // date strings
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+
+//timestamps
+const before = new Date("February 1 2019 7:30:59");
 const now = new Date();
-console.log(now);
-console.log(typeof now);
 
-console.log('getFullYear:', now.getFullYear());
-console.log('getMonth:', now.getMonth());       
-console.log('getDate:', now.getDate());
-console.log('getDay:', now.getDay());           
-console.log('getHours:', now.getHours());       
-console.log('getMinutes:', now.getMinutes());   
-console.log('getSeconds:', now.getSeconds());               
-console.log('getMilliseconds:', now.getMilliseconds());
-console.log('getTime:', now.getTime()); 
-
-// timestamps
-console.log('timestamp:', now.getTime());
-
-// date strings
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());  
-
+console.log("Before:", before.getTime());
